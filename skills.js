ff471cbb2354a6bb1b348f394f429bf6642c9885
Silviu-Mihnea Cucuiet <40487014+SilviuMihnea@users.noreply.github.com>
2021-02-12 22:24:57 +0200

    Add skills widget

---
+++ skills.js
@@ -0,0 +1,58 @@
+const skills = [
+    // languages
+    { "name": "java.png", "size": "large" },
+    { "name": "js.png", "size": "large" },
+    { "name": "ts.png", "size": "large" },
+    { "name": "csharp.png", "size": "medium" },
+    { "name": "kotlin.png", "size": "medium" },
+    { "name": "python.png", "size": "medium" },
+
+    // frameworks
+    { "name": "spring.png", "size": "large" },
+    { "name": "react.png", "size": "large" },
+    { "name": "angular.png", "size": "large" },
+    { "name": "android.png", "size": "medium" },
+    { "name": "dotnet.png", "size": "medium" },
+    { "name": "node.png", "size": "medium" },
+    { "name": "spring_cloud.png", "size": "medium" },
+    { "name": "flask.png", "size": "small" },
+    { "name": "django.png", "size": "small" },
+    { "name": "unity.png", "size": "small" },
+    
+    
+    // specific libraries
+    { "name": "redux.png", "size": "medium" },
+    { "name": "swagger.png", "size": "medium" },
+    
+    // tools
+    { "name": "github.png", "size": "large" },
+    { "name": "gradle.png", "size": "large" },
+    { "name": "git.png", "size": "medium" },
+    { "name": "heroku.png", "size": "medium" },
+    { "name": "jira.png", "size": "medium" },
+    { "name": "maven.png", "size": "medium" },
+    { "name": "npm.png", "size": "medium" },
+    { "name": "jenkins.png", "size": "small" },
+
+    // cloud
+    { "name": "azure.png", "size": "small" },
+    { "name": "aws.png", "size": "small" },
+    { "name": "google_cloud.png", "size": "medium" },
+    { "name": "docker.png", "size": "medium" },
+    { "name": "kubernetes.png", "size": "medium" },
+
+    // testing
+    { "name": "cucumber.png", "size": "medium" },
+    { "name": "cypress.png", "size": "medium" },
+
+    // architecture
+    { "name": "graphql.png", "size": "small" },
+    { "name": "kafka.png", "size": "small" },
+    { "name": "rabbitmq.png", "size": "medium" },
+
+    // database
+    { "name": "postgres.png", "size": "large" },
+    { "name": "mongo.png", "size": "medium" },
+    { "name": "mysql.png", "size": "medium" },
+    { "name": "redis.png", "size": "small" },
+];
